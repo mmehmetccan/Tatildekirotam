c76bdcd9e130618312605e6bec19e34a16601add
mmehmetccan <142463298+mmehmetccan@users.noreply.github.com>
2025-09-03 13:58:45 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Mobil menü
     const menuToggles = document.querySelectorAll('.menu-toggle');
     const mobileMenu = document.querySelector('.mobile-menu');
     const body = document.body;
@@ -12,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Mobil menü dropdownları
     const mobileDropdownToggles = document.querySelectorAll('.has-submenu-mobile > span');
 
     mobileDropdownToggles.forEach(toggle => {
@@ -24,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Sayfa dışına tıklanınca menüyü kapatma
     document.addEventListener('click', function(event) {
         if (!mobileMenu.contains(event.target) && !Array.from(menuToggles).some(toggle => toggle.contains(event.target))) {
             if (mobileMenu.classList.contains('active')) {
@@ -34,47 +31,36 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-    // Sayfa yüklendiğinde bu kod çalışır
     document.addEventListener('DOMContentLoaded', function() {
-        // Gerekli HTML elemanlarını seç
         const imageModal = document.getElementById('imageModal');
         const modalImage = document.getElementById('modalImage');
         const closeButton = document.querySelector('.close-button');
 
-        // Tüm .modal-trigger elemanlarını seç
         const imageTriggers = document.querySelectorAll('.modal-trigger');
 
-        // Her bir görsel tetikleyicisine tıklama olayını dinle
         imageTriggers.forEach(trigger => {
             trigger.addEventListener('click', function() {
-                // Tıklanan elemanın data-src özelliğini al
                 const fullImageUrl = this.getAttribute('data-src');
 
-                // Modal görselinin src özelliğini ayarla
                 modalImage.src = fullImageUrl;
 
-                // Modal pencereyi görünür yap
                 imageModal.classList.add('visible');
             });
         });
 
-        // Kapatma butonu veya modalın dışına tıklanınca modalı gizle
         function closeModal() {
             imageModal.classList.remove('visible');
-            // Modalın içeriğini temizle (isteğe bağlı)
             modalImage.src = '';
         }
 
         closeButton.addEventListener('click', closeModal);
 
         imageModal.addEventListener('click', function(event) {
-            // Sadece modalın dışına tıklanırsa kapat
             if (event.target === imageModal) {
                 closeModal();
             }
         });
 
-        // ESC tuşuna basıldığında modalı kapat
         document.addEventListener('keydown', function(event) {
             if (event.key === 'Escape' && imageModal.classList.contains('visible')) {
                 closeModal();
